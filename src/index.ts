67b78d4abc076e28edb9bde9abc95beb50ae5cdb
Jason Belmonti <jasonbelmonti@gmail.com>
2026-05-15 13:41:54 -0500

    Scaffold markdown-context package

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export type {
+  ContextDiagnostic,
+  ContextLinkCandidate,
+  RepoPathLensArtifact,
+  RepoPathSourceIdentity,
+  ResolveResult,
+  ScanResult,
+  SourceRange,
+  ValidateResult,
+} from "./core/types.js";
---
+++ vitest.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    exclude: ["**/.worktrees/**", "**/dist/**", "**/node_modules/**"],
+  },
+});
